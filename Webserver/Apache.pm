334439a25c346daa045fa2a5470cdc6293badbb4
Jan <krimdomu@googlemail.com>
2012-02-16 00:06:22 +0100

    added a simple Apache install module

---
+++ Webserver/Apache.pm
@@ -0,0 +1,127 @@
+#
+# AUTHOR: jan gehring <jan.gehring@gmail.com>
+# REQUIRES: 
+# LICENSE: GPLv3
+# 
+# Simple Module to install Apache on your Server.
+
+package Webserver::Apache;
+
+
+use Rex -base;
+
+# some package-wide variables
+
+our %package = (
+   Debian => "apache2",
+   Ubuntu => "apache2",
+   CentOS => "httpd",
+   Mageia => "apache-base",
+);
+
+our %service_name = (
+   Debian => "apache2",
+   Ubuntu => "apache2",
+   CentOS => "httpd",
+   Mageia => "httpd",
+);
+
+task "setup", sub {
+
+   my $pkg     = $package{get_operating_system()};
+   my $service = $service_name{get_operating_system()};
+
+   # install apache package
+   install package => $pkg;
+
+   # ensure that apache is started
+   service $service => "ensure" => "started";
+
+};
+
+task "start", sub {
+
+   my $service = $service_name{get_operating_system()};
+   service $service => "start";
+
+};
+
+task "stop", sub {
+
+   my $service = $service_name{get_operating_system()};
+   service $service => "stop";
+
+};
+
+task "restart", sub {
+
+   my $service = $service_name{get_operating_system()};
+   service $service => "restart";
+
+};
+
+task "reload", sub {
+
+   my $service = $service_name{get_operating_system()};
+   service $service => "reload";
+
+};
+
+1;
+
+=pod
+
+=head2 Module to install Apache
+
+This module installs apache webserver.
+
+=head2 USAGE
+
+Put it in your I<Rexfile>
+
+ # your tasks
+ task "one", sub {};
+ task "two", sub {};
+    
+ require Webserver::Apache;
+
+And call it:
+
+ rex -H $host Webserver:Apache:setup
+
+Or, to use it as a library
+
+ task "yourtask", sub {
+    Webserver::Apache::setup();
+ };
+   
+ require Webserver::Apache;
+
+=head2 TASKS
+
+=over 4
+
+=item setup
+
+This task will install apache httpd.
+
+=item start
+
+This task will start the apache daemon.
+
+=item stop
+
+This task will stop the apache daemon.
+
+=item restart
+
+This task will restart the apache daemon.
+
+=item reload
+
+This task will reload the apache daemon.
+
+=back
+
+=cut
+
