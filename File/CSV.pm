69eabf823b0b34c6d70482cde193beaeacff0e8a
Jan Gehring <krimdomu@googlemail.com>
2012-04-17 15:16:28 +0200

    added simple CSV module

---
+++ File/CSV.pm
@@ -0,0 +1,53 @@
+#
+# AUTHOR: jan gehring <jan.gehring@gmail.com>
+# REQUIRES: 
+# LICENSE: Apache License 2.0
+# 
+# This is a small Module to parse simple comma seperated
+# text files (csv).
+
+package File::CSV;
+   
+use strict;
+use warnings;
+
+require Exporter;
+
+use vars qw(@EXPORT);
+@EXPORT = qw(read_csv_file);
+use base qw(Exporter);
+
+sub read_csv_file {
+   my ($file, $code) = @_;
+   open(my $fh, "<", $file) or die($!);
+   while(my $line = <$fh>) {
+      chomp $line;
+      my @cols = split(m/,(?!(?:[^",]|[^"],[^"])+")/, $line);
+      &$code(@cols);
+   }
+   close($fh);
+}
+
+1;
+
+=pod
+
+=head2 Simple CSV Module
+
+This is a simple module to read csv files.
+
+=head2 USAGE
+
+You can use this module as a library. It will export the function I<read_csv_file>.
+
+ use File::CSV;
+    
+ task "yourtask", sub {
+    read_csv_file "yourfile.csv", sub {
+       my ($col1, $col2, $col3, ...) = @_;
+       print "col1: $col1\n";
+       print "col2: $col2\n";
+       print "col3: $col3\n";
+    };
+ };
+
