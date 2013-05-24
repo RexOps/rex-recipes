197a7a4fcc95d1bdf529622c894297c2bc743c1b
Jan Gehring <jan.gehring@inovex.de>
2013-05-24 11:59:12 +0200

    added create_host function

--- Rex/Cloud/OpenNebula/RPC.pm
+++ Rex/Cloud/OpenNebula/RPC.pm
@@ -151,6 +151,22 @@ sub create_vm {
    return $self->get_vm($res);
 }
 
+sub create_host {
+   my ($self, %option) = @_;
+
+   my $data = $self->_rpc("one.host.allocate",
+                              [ string => $option{name} ],
+                              [ string => $option{im_mad} ],
+                              [ string => $option{vmm_mad} ],
+                              [ string => $option{vnm_mad} ],
+                              [ int => (exists $option{cluster} ? $option{cluster} : -1) ] );
+   if(ref($data) ne "ARRAY") {
+      return;
+   }
+
+   return $self->get_host($data->[1]);
+}
+
 sub _rpc {
    my ($self, $meth, @params) = @_;                                                                                
 
