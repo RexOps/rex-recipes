521bd1ac5fc99e6f9d0c317a211646acfc1ea4f9
melezhik <melezhik@gmail.com>
2016-03-21 17:08:52 +0300

    prepare/setup/configure/run

--- Rex/Misc/Sparrow/DiskCheck/__module__.pm
+++ Rex/Misc/Sparrow/DiskCheck/__module__.pm
@@ -8,11 +8,12 @@ task prepare => sub {
    my ( $params ) = @_;
 
    install package => 'curl';
+   install package => 'perl-devel';
    
    my $output = run "curl -fkL http://cpanmin.us/ -o /bin/cpanm && chmod +x /bin/cpanm";  
    say $output;
 
-   my $output = run "cpanm Sparrow";
+   my $output = run "cpanm Test::More Sparrow";
    say $output;
   
 
@@ -22,15 +23,42 @@ task setup => sub {
 
    my ( $params ) = @_;
 
-   my $output = run "export PATH=/usr/local/bin/PATH:\$PATH; sparrow index update && sparrow install df-check";  
+   my $output = run "sparrow index update && sparrow plg install df-check";  
+
    say $output;
 
 };
 
-#task setup => sub {
-#   my $output = run "hostname -f";
-#   say $output;
-#};
+task configure => sub {
+
+   my ( $params ) = @_;
+
+   file "/tmp/sparrow-df-check.ini",
+      content   => template("files/etc/ntp.conf", threshold => $params->{threshold} || 80 ),
+   ;
+
+   my $output = run "sparrow project create system";  
+   say $output;
+
+   my $output = run "sparrow check add system disk";  
+   say $output;
+
+   my $output = run "sparrow check set system disk df-check";  
+   say $output;
+
+   my $output = run "sparrow check load_ini system disk /tmp/sparrow-df-check.ini";  
+   say $output;
+
+   my $output = run "sparrow check show system disk";  
+   say $output;
+
+
+};
+
+task run => sub {
+   my $output = run "sparrow plg run df-check";
+   say $output;
+};
 
 1;
 
